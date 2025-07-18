5170fca94a5e6e9bae9c833eeed2e3dca0c86fd8
Henrique Viana <henriquehvisa@gmail.com>
2025-07-18 12:16:59 -0300

    feat: WIP - create Dashboard e Login page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,73 @@
-import { useAuth } from "../../context/AuthContext";
+import Sidebar from "../../components/Sidebar";
+import { PieChart } from "../../components/Charts/Pie";
+import { BarChart } from "../../components/Charts/Bar";
+
+const COLORS = ["#3b82f6", "#f97316", "#10b981"];
 
 const Dashboard = () => {
-  const { user } = useAuth();
+  // Exemplo de dados mockados
+  const statusData = [
+    { name: "Pendente", value: 5 },
+    { name: "Em Progresso", value: 8 },
+    { name: "Concluído", value: 12 },
+  ];
 
-  return <h1>Dash</h1>;
+  const categoryData = [
+    { name: "Trabalho", value: 10 },
+    { name: "Pessoal", value: 7 },
+    { name: "Outros", value: 8 },
+  ];
+
+  const totalTasks = statusData.reduce((sum, item) => sum + item.value, 0);
+  const completedTasks =
+    statusData.find((item) => item.name === "Concluído")?.value || 0;
+  const pendingTasks = totalTasks - completedTasks;
+
+  return (
+    <div className="flex min-h-screen h-screen bg-gray-50">
+      <Sidebar />
+      <div className="p-6 bg-gray-50 min-h-screen flex-1">
+        <h1 className="text-2xl font-bold mb-6 text-default-black">
+          Dashboard de Tarefas
+        </h1>
+
+        {/* KPI Section */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
+          <div className="bg-white rounded-xl p-4 shadow">
+            <h2 className="text-lg font-semibold mb-2 text-default-black">
+              Tarefas Concluídas
+            </h2>
+            <p className="text-3xl font-bold text-green-600">
+              {completedTasks}
+            </p>
+          </div>
+          <div className="bg-white rounded-xl p-4 shadow">
+            <h2 className="text-lg font-semibold mb-2 text-default-black">
+              Tarefas Pendentes (Pendentes + Em Progresso)
+            </h2>
+            <p className="text-3xl font-bold text-red-500">{pendingTasks}</p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="bg-white rounded-xl p-4 shadow">
+            <h2 className="font-semibold mb-2 text-default-black">
+              Distribuição por Status
+            </h2>
+            <PieChart data={statusData} colors={COLORS} />
+          </div>
+
+          {/* Category Distribution Bar Chart */}
+          <div className="bg-white rounded-xl p-4 shadow">
+            <h2 className="font-semibold mb-2 text-default-black">
+              Distribuição por Categoria
+            </h2>
+            <BarChart data={categoryData} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,59 +1,34 @@
-// import { useAuth } from '../context/AuthContext';
-// import { useNavigate } from 'react-router-dom';
-import { useCallback, useRef, useState, type FormEvent } from "react";
-import { useAuth } from "../../context/AuthContext";
 import Form from "../../components/Form";
 import Input from "../../components/Input";
-import { useNavigate } from "react-router-dom";
-
-type UserType = {
-  email: string;
-  password: string;
-};
-
-const userLocalRef: UserType = {
-  email: "",
-  password: "",
-};
+import { useLogin } from "../../hooks/useLogin";
 
 const Login = () => {
-  const { setUser } = useAuth();
-  const navigate = useNavigate();
-
-  const updateUser = useCallback((field: keyof UserType, value: string) => {
-    userLocalRef[field] = value;
-  }, []);
-
-  const handleSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setUser(userLocalRef);
-    navigate("/dashboard");
-  }, []);
+  const { handleSubmit, updateUser } = useLogin();
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <Form
         onSubmit={handleSubmit}
-        className="bg-white p-6 rounded-xl shadow-xl"
+        className="bg-white p-6 rounded-xl shadow-xl max-w-lg min-w-lg"
       >
-        <h2 className="text-xl font-bold mb-4 text-default-black">
+        <h2 className="text-xl font-bold text-default-black">
           Acessar o Sistema
         </h2>
         <Input
           type="email"
           placeholder="E-mail"
           onValueChange={(value) => updateUser("email", value)}
-          className="w-full mb-4 border rounded p-2 text-default-black"
+          className="w-full mt-4 border rounded p-2 text-default-black"
         />
         <Input
           type="password"
           placeholder="Senha"
           onValueChange={(value) => updateUser("password", value)}
-          className="w-full mb-4 border rounded p-2 text-default-black"
+          className="w-full mt-4 border rounded p-2 text-default-black"
         />
         <button
           type="submit"
-          className="bg-blue-500 text-white w-full rounded p-2"
+          className="bg-blue-500 mt-4 text-white w-full rounded p-2"
         >
           Entrar
         </button>
