4417e5ead1bf0903a8d47569086cde6c76bce829
Henrique Viana <henriquehvisa@gmail.com>
2025-07-21 18:18:11 -0300

    refactor: remove unused code

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,29 +1,12 @@
-import { useStore } from "../../hooks/useStore";
-import { useDispatch } from "react-redux";
-// import { deleteTask } from "../../store/tasksSlice"; // supondo que você tenha essa action
 import TaskList from "../../components/Tasks/TaskList";
 import Sidebar from "../../components/Sidebar";
+import { useTasks } from "../../hooks/useTasks";
 
 const TasksPage = () => {
-  const { tasks } = useStore();
-  const dispatch = useDispatch();
-
-  const handleDelete = (id: string) => {
-    //dispatch(deleteTask(id)); // sua action personalizada
-  };
-
-  const handleEdit = (id: string) => {
-    console.log("Editar tarefa", id);
-    // redirecionar ou abrir modal
-  };
-
-  const handleNewTask = () => {
-    console.log("Criar nova tarefa");
-    // redirecionar ou abrir modal
-  };
+  const { redirectToCreateTask } = useTasks();
 
   return (
-    <div className="flex min-h-screen h-screen bg-gray-50">
+    <section className="flex min-h-screen h-screen bg-gray-50">
       <Sidebar />
       <div className="p-6 w-full">
         <div className="flex justify-between items-center mb-4">
@@ -31,16 +14,16 @@ const TasksPage = () => {
             Minhas Tarefas
           </h1>
           <button
-            onClick={handleNewTask}
+            onClick={redirectToCreateTask}
             className="px-4 py-2 rounded-xl bg-green-600 text-white hover:bg-green-700"
           >
             Nova Tarefa
           </button>
         </div>
 
-        <TaskList tasks={tasks} onEdit={handleEdit} onDelete={handleDelete} />
+        <TaskList />
       </div>
-    </div>
+    </section>
   );
 };
 
