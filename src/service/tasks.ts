90a9adb1d6fa840bbdd6bab60782600e4e5f8540
Henrique Viana <henriquehvisa@gmail.com>
2025-07-21 18:18:32 -0300

    refactor: update crud services

--- src/service/tasks.ts
+++ src/service/tasks.ts
@@ -1,10 +1,30 @@
 import { api } from "../config/api"
+import type { TaskType } from "../store/tasksSlice"
 
 type GetTasksType = {
   userId: string
 }
 
+type CreateTaskType = {
+  userId: string
+  body: {
+    items: TaskType[]
+  }
+}
+
 export const getTasks = async ({ userId }: GetTasksType) => {
   return await api.get(`/tasks?userId=${userId}`)
+}
+
+export const createTask = async ({ userId, body }: CreateTaskType) => {
+  return await api.patch(`/tasks/${userId}`, body)
+}
+
+export const updateTask = async ({ userId, body }: CreateTaskType) => {
+  return await api.patch(`/tasks/${userId}`, body)
+}
+
+export const deleteTask = async ({ userId, body }: CreateTaskType) => {
+  return await api.patch(`/tasks/${userId}`, body)
 }
 
