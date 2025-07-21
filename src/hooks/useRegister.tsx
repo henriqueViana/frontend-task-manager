4ae4f13dd67d10c22f039c55209789ed1b05381f
Henrique Viana <henriquehvisa@gmail.com>
2025-07-21 18:17:13 -0300

    refactor: create crud function to tasks  in hook

--- src/hooks/useRegister.tsx
+++ src/hooks/useRegister.tsx
@@ -1,14 +1,26 @@
 import { useCallback, type FormEvent } from "react";
-import type { TaskType } from "../store/tasksSlice";
-import { useNavigate } from "react-router-dom";
+import {
+  addTask,
+  removeTask,
+  updateTask,
+  type TaskType,
+} from "../store/tasksSlice";
+import { useLocation, useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import { useAuth } from "../context/AuthContext";
+import * as service from "../service/tasks";
+import { useStore } from "./useStore";
+import { useDispatch } from "react-redux";
+import { useTasks } from "./useTasks";
 
 type UseRegisterType = {
   updateFieldRegister: (field: keyof TaskType, value: string) => void;
   handleSubmit: (event: FormEvent<HTMLFormElement>) => void;
+  deleteTask: (taskId: string) => void;
+  task: TaskType;
 };
 
-const task: TaskType = {
+let task: TaskType = {
   category: "",
   description: "",
   priority: "",
@@ -17,8 +29,20 @@ const task: TaskType = {
   title: "",
 };
 
+const extractTaskIdOfPath = () => {
+  const location = useLocation();
+  return location.pathname.split("/").pop() || "";
+};
+
 export const useRegister = (): UseRegisterType => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { user } = useAuth();
+  const { tasks } = useStore();
+  const { getTaskById } = useTasks();
+
+  const taskIdPath = extractTaskIdOfPath();
+  task = getTaskById(taskIdPath) || task;
 
   const updateFieldRegister = useCallback(
     (field: keyof TaskType, value: string) => {
@@ -27,15 +51,74 @@ export const useRegister = (): UseRegisterType => {
     []
   );
 
+  const editTask = () => {
+    const updatedItems = tasks.map((item: TaskType) =>
+      item.id === task.id ? { ...task } : item
+    );
+
+    const body = {
+      items: updatedItems,
+    };
+
+    dispatch(updateTask(task));
+
+    service.updateTask({
+      userId: user.id,
+      body,
+    });
+
+    navigate("/tarefas");
+  };
+
+  const createNewTask = () => {
+    task.id = uuidv4();
+
+    const body = {
+      items: [...tasks, task],
+    };
+
+    dispatch(addTask(task));
+
+    service.createTask({
+      userId: user.id,
+      body,
+    });
+  };
+
+  const deleteTask = (taskId: string) => {
+    const updatedItems = tasks.filter((item: TaskType) => item.id !== taskId);
+
+    const body = {
+      items: updatedItems,
+    };
+
+    dispatch(removeTask(taskId));
+
+    service.deleteTask({
+      userId: user.id,
+      body,
+    });
+
+    navigate("/tarefas");
+  };
+
   const handleSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    task.id = uuidv4();
-    console.log("event form", task);
-    //navigate("/tarefa");
+
+    if (task.id) {
+      editTask();
+      navigate("/tarefas");
+      return;
+    }
+
+    createNewTask();
+    navigate("/tarefas");
   }, []);
 
   return {
     updateFieldRegister,
     handleSubmit,
+    deleteTask,
+    task,
   };
 };
